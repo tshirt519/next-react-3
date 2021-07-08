ad93a0af35c8b64dd3a5b0c462eccabc1a3e511c
tshirt519 <tshirt519@gmail.com>
2021-07-08 16:27:36 +0900

    ♻️ Headline部分をcomponent化

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,6 @@
 import styles from '../styles/Home.module.css';
 import Image from 'next/image'
 
-
 export function Footer() {
   return (
       <footer className={styles.footer}>
--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Footer } from "../components/Footer" 
 import { Links } from "../components/Links"
+import { Headline } from '../components/Headline'
 
 export default function Home() {
   return (
@@ -14,17 +15,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          About Page
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/about.js</code>
-        </p>
-
+        <Headline page="about" />
         <Links />
-
       </main>
       
       <Footer />
