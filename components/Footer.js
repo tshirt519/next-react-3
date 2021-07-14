4523fbd1bc00d18bee96fb3aeb1e109e90539975
tshirt519 <tshirt519@gmail.com>
2021-07-14 15:48:58 +0900

    💄 スタイルファイルの分割

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
-import styles from '../styles/Home.module.css';
+import classes from './Footer.module.css';
 import Image from 'next/image'
 
 export function Footer() {
   return (
-      <footer className={styles.footer}>
+      <footer className={classes.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
--- components/Main.js
+++ components/Main.js
@@ -1,13 +1,13 @@
- import styles from '../styles/Home.module.css'
+ import classes from './Main.module.css'
 import { Links } from "./Links"
 import { Headline } from './Headline'
 
 export function Main(props) {
   return (
     <div>
-      <main className={styles.main}>
+      <main className={classes.main}>
         <Headline page={props.page} >
-          <code className={styles.code}>pages/{props.page}.js</code>
+          <code className={classes.code}>pages/{props.page}.js</code>
         </Headline>
         <Links />
       </main>
