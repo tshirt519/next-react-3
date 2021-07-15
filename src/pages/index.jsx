8a94ed7003ecc8a26c172156dc0a0e37257c44ec
tshirt519 <tshirt519@gmail.com>
2021-07-15 16:07:58 +0900

    ⚗️ onclickイベントテスト

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,17 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
+import { useCallback } from 'react';
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +22,7 @@ export default function Home() {
       </Head>
       <Header />
       <Main page="index" />
+      <a href="/about" onClick={handleClick}>ボタン</a>
 
       <Footer />
     </div>
