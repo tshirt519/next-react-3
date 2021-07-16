9db21eaa58357539bcf7846e79b0f09720bc9c3e
tshirt519 <tshirt519@gmail.com>
2021-07-16 15:00:52 +0900

    ✨ useEffectテスト

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import Link from "next/link";
 
 export default function Home() {
@@ -13,6 +13,16 @@ export default function Home() {
     console.log(e.target.href);
     e.preventDefault();
     alert(foo);
+  }, []);
+
+  useEffect(() => {
+    console.log("マウント時")
+    document.body.style.backgroundColor = "lightblue";
+  
+    return () => {
+      console.log("アンマウント時")
+      document.body.style.backgroundColor = "";
+    }
   }, []);
 
   return (
