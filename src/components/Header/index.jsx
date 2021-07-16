222960579ed2bb747e5c45b5f9e1b3df552471d9
tshirt519 <tshirt519@gmail.com>
2021-07-16 16:44:01 +0900

    .containerのheight初期設定解除

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,13 +3,13 @@ import classes from 'src/components/Header/Header.module.css';
 
 export function Header() {
   return (
-      <header className={classes.header}>
-        <Link href="/">
-          <a className={classes.anchor}>Index</a>
-        </Link>
-        <Link href="/about">
-          <a className={classes.anchor}>About</a>
-        </Link>
-      </header>
+    <header className={classes.header}>
+      <Link href="/">
+        <a className={classes.anchor}>Index</a>
+      </Link>
+      <Link href="/about">
+        <a className={classes.anchor}>About</a>
+      </Link>
+    </header>
   );
 }
