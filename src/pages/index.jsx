b44b9c4de8560d85e4d83e3c856c2d77cf55dabd
tshirt519 <tshirt519@gmail.com>
2021-07-16 16:38:12 +0900

    ⚡️ useCallback, useEffectの第二引数追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,16 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useEffect, useState } from 'react';
-import Link from "next/link";
+import { useEffect, useState, useCallback } from 'react';
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount(count => count + 1);
-    setCount(count => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -30,6 +30,7 @@ export default function Home() {
       </Head>
       <Header />
       <Main page="index" />
+      
       <h1>{count}</h1>
       <button onClick={handleClick}>Button</button>
 
