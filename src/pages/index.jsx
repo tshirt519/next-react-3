5aa9077385898e122c95673fcad9847caefea4ed
tshirt519 <tshirt519@gmail.com>
2021-07-16 16:07:43 +0900

    ✨ counterを設置してuseStateでカウントアップ

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,24 +3,21 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useCallback, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Link from "next/link";
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+  };
 
   useEffect(() => {
-    console.log("マウント時")
     document.body.style.backgroundColor = "lightblue";
-  
+
     return () => {
-      console.log("アンマウント時")
       document.body.style.backgroundColor = "";
     }
   }, []);
@@ -33,10 +30,8 @@ export default function Home() {
       </Head>
       <Header />
       <Main page="index" />
-      
-      <Link href="/about">
-        <a onClick={handleClick}>Button</a>
-      </Link>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>Button</button>
 
       <Footer />
     </div>
