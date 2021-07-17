4000baf3fbc190c38ce9af525318515d42b26726
tshirt519 <tshirt519@gmail.com>
2021-07-17 16:27:47 +0900

    ✨ useStateで配列をテスト

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ import { useEffect, useState, useCallback } from 'react';
 export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true)
+  const [array, setArray] = useState([]);
   
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -28,6 +28,15 @@ export default function Home() {
     setText(e.target.value.trim());
   }, []);
 
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素が既に存在しています");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
@@ -48,6 +57,12 @@ export default function Home() {
       <button onClick={handleClick}>カウント</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
       <Footer />
     </div>
   );
