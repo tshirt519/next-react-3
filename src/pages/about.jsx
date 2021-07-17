37b641b32ab658987ba05720e042b100e496fdbb
tshirt519 <tshirt519@gmail.com>
2021-07-17 17:54:24 +0900

    🎨 Custom hooksを使った機能のまとめ

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,8 +3,15 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
+import { useCounter } from 'src/hooks/useCounter';
+import { useInputArray } from 'src/hooks/useInputArray';
+import { useBgLightBlue } from 'src/hooks/useBgLightBlue';
 
-export default function Home() {
+export default function About() {
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +21,18 @@ export default function Home() {
       
       <Header />
       <Main page="about" />
+
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>カウント</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
       
       <Footer />
     </div>
