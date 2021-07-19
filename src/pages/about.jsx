806d52af400f1ccc970f78d78f5e9a26dac1d62d
tshirt519 <tshirt519@gmail.com>
2021-07-19 16:57:51 +0900

    ♻️ リファクタリング

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,16 +4,7 @@ import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
 
-export default function About({
-    doubleCount,
-    isShow, 
-    handleClick, 
-    handleDisplay, 
-    text, 
-    array, 
-    handleChange, 
-    handleAdd
-  }) {
+const About = (props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -24,15 +15,15 @@ export default function About({
       <Header />
       <Main page="about" />
 
-      {isShow ? <h1>{doubleCount}</h1> : null}
+      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
 
-      <button onClick={handleClick}>カウント</button>
-      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <button onClick={props.handleClick}>カウント</button>
+      <button onClick={props.handleDisplay}>{props.isShow ? "非表示" : "表示"}</button>
 
-      <input type="text" value={text} onChange={handleChange} />
-      <button onClick={handleAdd}>追加</button>
+      <input type="text" value={props.text} onChange={props.handleChange} />
+      <button onClick={props.handleAdd}>追加</button>
       <ul>
-        {array.map(item => {
+        {props.array.map(item => {
           return <li key={item}>{item}</li>
         })}
       </ul>
@@ -41,3 +32,5 @@ export default function About({
     </div>
   );
 }
+
+export default About;
