c89f29914d9f964962ac59b98103f79879cadb5e
tshirt519 <tshirt519@gmail.com>
2021-08-10 16:37:58 +0900

    🚚 ファイルシステムルーティングをテスト

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,8 @@
 import { usePosts } from "src/hooks/usePosts";
+import Link from "next/link";
 
 export const Posts = () => {
   const { data, error, isLoading, isEmpty } = usePosts();
-
-  console.log({ data, error });
 
   if (isLoading) {
     return <div>ローディング中です</div>;
@@ -18,7 +17,13 @@ export const Posts = () => {
   return (
     <ol>
       {data.map((post) => {
-        return <li key={post.id}>{post.title}</li>;
+        return (
+          <li key={post.id}>
+            <Link href={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        );
       })}
     </ol>
   );
