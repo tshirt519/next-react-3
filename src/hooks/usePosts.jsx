28f7d0281dea82677f6ac44fede2fe9e148e6245
tshirt519 <tshirt519@gmail.com>
2021-08-10 17:34:50 +0900

    ✨ ユーザーのfetch, fetcherの共通化

--- src/hooks/usePosts.jsx
+++ src/hooks/usePosts.jsx
@@ -1,14 +1,5 @@
+import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
-
-const fetcher = async (url) => {
-  const response = await fetch(url);
-
-  if (!response.ok) {
-    throw new Error("エラーが発生したため、データの取得に失敗しました。");
-  }
-  const json = await response.json();
-  return json;
-};
 
 // useSWRを用いたカスタムフック→条件式に変数を入れることで明確に
 // コンポーネント間で使い回すときにも便利
@@ -17,6 +8,7 @@ export const usePosts = () => {
     "https://jsonplaceholder.typicode.com/posts",
     fetcher
   );
+
   return {
     data,
     error,
