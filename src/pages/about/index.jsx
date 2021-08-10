677a5831db43b382d277335653b408d11eaceef1
tshirt519 <tshirt519@gmail.com>
2021-08-10 16:45:41 +0900

    🐛  jsconfig元に戻す

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
-import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { Main } from "src/components/Main";
 
 const About = (props) => {
   return (
