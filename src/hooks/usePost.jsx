9264672ebbce1646b054aadc3c71eaf13c54ff9a
tshirt519 <tshirt519@gmail.com>
2021-08-11 18:41:20 +0900

    ✨ 投稿の詳細ページにコメントを表示

--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -3,21 +3,15 @@ import useSWR from "swr";
 
 export const usePost = () => {
   const router = useRouter();
-  const { data: post, error: postError } = useSWR(
+  const { data, error } = useSWR(
     router.query.id
       ? `https://jsonplaceholder.typicode.com/posts/${router.query.id}`
-      : null,
-  );
-  const { data: user, error: userError } = useSWR(
-    post?.userId
-      ? `https://jsonplaceholder.typicode.com/users/${post.userId}`
-      : null,
+      : null
   );
 
   return {
-    post,
-    user,
-    error: postError || userError,
-    isLoading: !user && !userError,
+    data,
+    error,
+    isLoading: !data && !error,
   };
 };
